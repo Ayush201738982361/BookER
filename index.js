2b7a1d04759477529924899748704a6f40930eeb
Ayush201738982361 <ayushnair87@gmail.com>
2024-12-18 14:15:36 +0530

    Made route for signup

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+app.get("/user/signup", (req, res) => {
+  // res.sendFile(path.join(__dirname, "views", "signup.html"));
+  res.end("Men at work");
+});
+
 app.listen(800, () => {
   console.log(`Server is running on port 800`);
 });
