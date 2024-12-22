c6098417db5450eb1c3c1bcb21be97d3c18aacd5
Ayush201738982361 <ayushnair87@gmail.com>
2024-12-22 11:31:26 +0530

    Connected mongodb and added static router

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const path = require("path");
+const staticRouter = require("./routes/staticRouter");
+const { connectDB } = require("./connection");
 
 const app = express();
 app.use(express.static(path.join(__dirname, "views")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+connectDB("mongodb://127.0.0.1:27017/BookER").then(() => {
+  console.log("MongoDB Connected");
 });
 
-app.get("/signup", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "signup.html"));
-});
+app.use("/", staticRouter);
 
 app.listen(800, () => {
   console.log(`Server is running on port 800`);
