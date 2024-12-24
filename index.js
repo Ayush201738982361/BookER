fa706ad8f3e63f31ee5046ff576ffff67d23de65
Ayush201738982361 <ayushnair87@gmail.com>
2024-12-24 11:15:38 +0530

    Changed HTML To EJS for server side rendering

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const path = require("path");
 const staticRouter = require("./routes/staticRouter");
+const userRoutes = require("./routes/user");
 const { connectDB } = require("./connection");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "views")));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 connectDB("mongodb://127.0.0.1:27017/BookER").then(() => {
   console.log("MongoDB Connected");
 });
 
 app.use("/", staticRouter);
+app.use("/user", userRoutes);
 
 app.listen(800, () => {
   console.log(`Server is running on port 800`);
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views", "index.html"));
+  res.render("index");
 });
 
 router.get("/signup", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views", "signup.html"));
+  res.render("signup");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 module.exports = router;
