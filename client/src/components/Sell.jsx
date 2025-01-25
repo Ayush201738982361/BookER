bfb15e6de35b2d7fbfbda3b71d50cdb026706c28
Ayush201738982361 <ayushnair87@gmail.com>
2025-01-26 00:08:05 +0530

    Created Sell component for book listing

--- client/src/components/Sell.jsx
+++ client/src/components/Sell.jsx
@@ -1,6 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
 import styles from "../public/styles/sell.module.css";
 
 function Sell() {
+  const [book_name, setBookName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [price, setPrice] = useState("");
+  const [seller_name, setSellerName] = useState("");
+  const [seller_contact, setSellerContact] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/user/sell", {
+        book_name,
+        author,
+        price,
+        seller_name,
+        seller_contact,
+      })
+      .then((response) => {
+        if (response.status === 201) {
+          toast.success("Your listing has been added successfully");
+          setTimeout(() => {
+            navigate("/");
+          }, 2000);
+        } else {
+          toast.error("Failed to list your book");
+        }
+      })
+      .catch((error) => {
+        toast.error("Internal server error");
+        console.log("Error in listing books:", error);
+      });
+  }
   return (
     <>
       <div className={styles.hero_image}>
@@ -17,13 +54,21 @@ function Sell() {
         <i>Fill This Form And List Your Books</i>
       </div>
       <div className={styles.container}>
-        <form className={styles.inputForm}>
+        <ToastContainer position="top-right" autoClose={4000} />
+        <form
+          className={styles.inputForm}
+          method="POST"
+          onSubmit={handleSubmit}
+        >
           <input
             className={styles.input}
             placeholder="Name Of The Book"
             type="text"
             id="book_name"
             name="book_name"
+            onChange={(e) => {
+              setBookName(e.target.value);
+            }}
             required
           />
 
@@ -33,6 +78,9 @@ function Sell() {
             type="text"
             id="author"
             name="author"
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
             required
           />
 
@@ -43,6 +91,9 @@ function Sell() {
             id="price"
             name="price"
             min="0"
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
             required
           />
 
@@ -52,6 +103,9 @@ function Sell() {
             type="text"
             id="sellerName"
             name="sellerName"
+            onChange={(e) => {
+              setSellerName(e.target.value);
+            }}
             required
           />
 
@@ -63,6 +117,9 @@ function Sell() {
             name="sellerContact"
             pattern="[0-9]{10}"
             maxLength="10"
+            onChange={(e) => {
+              setSellerContact(e.target.value);
+            }}
             required
           />
           <button type="submit" className="btn btn-success">
