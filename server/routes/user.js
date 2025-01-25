5abb7d488ab4bb90a607db1a7cddd8323082a552
Ayush201738982361 <ayushnair87@gmail.com>
2025-01-26 00:10:11 +0530

    Created server route for listing

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { createNewUser } = require("../controller/user");
 const { loginUser } = require("../controller/user");
+const { createNewListing } = require("../controller/book");
 const { checkForAuth } = require("../middleware/auth");
 
 router.post("/signup", createNewUser);
 router.post("/login", loginUser);
+router.post("/sell", createNewListing);
 router.use(checkForAuth);
 
 module.exports = router;
