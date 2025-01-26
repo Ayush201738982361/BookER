73d572cc709f304f053d7b3694591129e04ffd5b
Ayush201738982361 <ayushnair87@gmail.com>
2025-01-27 00:35:47 +0530

    Created a controller for fetching book data

--- server/controller/book.js
+++ server/controller/book.js
@@ -11,6 +11,17 @@ async function createNewListing(req, res) {
   }
 }
 
+async function getAllListings(req, res) {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error fetching listings" });
+  }
+}
+
 module.exports = {
   createNewListing,
+  getAllListings,
 };
