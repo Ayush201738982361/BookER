f1da7fc5d0b59ebd2ec78fa6d64d7e916b40279c
Ayush201738982361 <ayushnair87@gmail.com>
2025-01-28 09:28:02 +0530

    Added hero section to /buy

--- client/src/components/Buy.jsx
+++ client/src/components/Buy.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import styles from "../public/styles/buy.module.css";
 
 function Buy() {
   const [listings, setListings] = useState([]);
@@ -16,31 +17,44 @@ function Buy() {
   }, []);
 
   return (
-    <div
-      style={{
-        marginTop: "50px",
-        display: "flex",
-        justifyContent: "center",
-        textAlign: "center",
-      }}
-    >
-      <div
-        className="card"
-        style={{ width: "50rem", margin: "1px solid black" }}
-      >
-        <ul className="list-group list-group-flush">
-          {listings.map((listing) => (
-            <li className="list-group-item py-3" key={listing._id}>
-              <div>Book: {listing.book_name}</div>
-              <div>Author: {listing.author}</div>
-              <div>Price: ${listing.price}</div>
-              <div>Seller: {listing.seller_name}</div>
-              <div>Contact: {listing.seller_contact}</div>
-            </li>
-          ))}
-        </ul>
+    <>
+      <div className={styles.hero_image}>
+        <div className={styles.hero_text}>
+          <h1>Find Your Next Great Read</h1>
+          <p>
+            Discover a wide selection of books at unbeatable prices. From
+            timeless classics to the latest bestsellers, explore listings from
+            passionate readers and sellers just like you. Start your journey to
+            your next favorite book today!
+          </p>
+        </div>
       </div>
-    </div>
+      <div
+        style={{
+          marginTop: "50px",
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <div
+          className="card"
+          style={{ width: "50rem", margin: "1px solid black" }}
+        >
+          <ul className="list-group list-group-flush">
+            {listings.map((listing) => (
+              <li className="list-group-item py-3" key={listing._id}>
+                <div>Book: {listing.book_name}</div>
+                <div>Author: {listing.author}</div>
+                <div>Price: ${listing.price}</div>
+                <div>Seller: {listing.seller_name}</div>
+                <div>Contact: {listing.seller_contact}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
